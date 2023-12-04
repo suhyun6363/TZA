206482d0f1e8909f0825f249819bfff021227917
heesoo235 <heesoo235@gmail.com>
2023-12-04 13:40:43 +0900

    axios react

--- src/personal_color/WebcamCapture.js
+++ src/personal_color/WebcamCapture.js
@@ -3,6 +3,7 @@
 import React, { useRef, useEffect } from "react";
 import Webcam from "react-webcam";
 import "./WebcamCapture.css";
+import axios from 'axios';
 
 const WebcamCapture = ({ onCapture }) => {
   const webcamRef = useRef(null);
@@ -13,13 +14,26 @@ const WebcamCapture = ({ onCapture }) => {
     const overlayImage = document.getElementById("person-image");
   }, []);
 
-  const handleCaptureClick = () => {
+  const handleCaptureClick = async () => {
     if (webcamRef.current) {
       const imageSrc = webcamRef.current.getScreenshot();
-      onCapture(imageSrc);
+      try {
+        // 이미지를 서버로 전송
+        const formData = new FormData();
+        formData.append('image, imageScr');
+        const response = await axios.post("http://localhost:8000/api/upload/", {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+        // 성공적으로 전송된 경우 처리
+        console.log("이미지 업로드 성공:", response.data);
+      } catch (error) {
+        // 전송 실패한 경우 오류 처리
+        console.error("이미지 업로드 실패:", error);
+      }
     }
   };
-
   return (
     <div id="video-container">
       {/* mirrored 속성을 true로 설정하여 웹캠 피드를 좌우로 뒤집습니다. */}
