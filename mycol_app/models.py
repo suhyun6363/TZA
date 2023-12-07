cdb3dae7a5af8cea584f404b1fa2deebf397d44a
yeeunAhn <yeeun780@gmail.com>
2023-12-07 18:20:48 +0900

    이미지업로드가능

--- mycol_app/models.py
+++ mycol_app/models.py
@@ -3,28 +3,12 @@ from django.db import models
 
 # Create your models here.
 
-# 사용자 정의 함수를 만들어 이미지 파일 이름을 특정한 규칙으로 생성
-def upload_to(instance, filename):
-    # 모델 인스턴스를 저장하여 ID를 할당
-    instance.save()
 
-    base_filename, file_extension = os.path.splitext(filename)
-    new_filename = f"captured_image_{instance.id}{file_extension}"
-    return f"uploads/{new_filename}"
-
-# UploadedImage 모델 정의
-class UploadedImage(models.Model):
-    id = models.AutoField(primary_key=True)
-    image = models.ImageField(upload_to=upload_to)  # upload_to 함수 사용
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-
-
-    """
 class UploadedImage(models.Model):
     id = models.AutoField(primary_key=True)
     image = models.ImageField(upload_to='uploads/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
-    """
+
 
 
 
