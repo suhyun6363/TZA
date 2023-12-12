daffc124d61f02cf1e54938b027e597a7241fecf
heesoo235 <heesoo235@gmail.com>
2023-12-12 16:19:39 +0900

    result 수정

--- src/personal_color/Result.js
+++ src/personal_color/Result.js
@@ -10,7 +10,9 @@ const ImageComponent = () => {
       'http://127.0.0.1:8000/media/cluster_images/cluster_1.png',
       'http://127.0.0.1:8000/media/cluster_images/cluster_2.png',
       'http://127.0.0.1:8000/media/cluster_images/cluster_3.png',
+      'http://127.0.0.1:8000/media/face_only0.png',
       'http://127.0.0.1:8000/media/cluster_images/total_weighted_mean_color.png'
+      
     ];
 
     // 각 이미지 가져오기
