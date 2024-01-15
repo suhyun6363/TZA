4d469cb4a9ea29693138f4fc36e24ed3513c8fcb
gyubb <lgbin910@gmail.com>
2024-01-15 22:45:49 +0900

    졸프에서 쓰는 장고

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'mycol_app',
     'rest_framework',
     'corsheaders',
+    'yolov5'
 ]
 
 CHANNEL_LAYERS = {
@@ -131,8 +132,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -144,3 +143,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+#추가
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'config', 'yolov5', 'static'),
+]
--- mycol_app/serializers.py
+++ mycol_app/serializers.py
@@ -1,6 +1,6 @@
 # mycol_app/serializers.py
 from rest_framework import serializers
-from .models import Analysis, UploadedImage
+from .models import Analysis, UploadedImage, WearingDetect
 
 class UploadedImageSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,5 +10,10 @@ class UploadedImageSerializer(serializers.ModelSerializer):
 class AnalysisSerializer(serializers.ModelSerializer):
     class Meta:
         model = Analysis
+        fields = '__all__'
+
+class WearingDetectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WearingDetect
         fields = '__all__'
 
