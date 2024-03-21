ab8ffcc1561d49fdcc6f8ce684c8bbf3680be34a
gyubb <lgbin910@gmail.com>
2024-03-21 16:01:16 +0900

    드레이핑->첫페이지 버튼 추가

--- src/personal_color/Draping2.js
+++ src/personal_color/Draping2.js
@@ -1,71 +1,71 @@
-// Draping2.js 파일
-
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Draping2.css";
 
-const SecondImageComponent = () => { // 이름을 변경함
+const SecondImageComponent = () => {
   const [imageSrcList, setImageSrcList] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    // Django 서버에 있는 이미지들의 URL 배열
-    const imageUrls = [];
-
-    for (let i = 1; i <= 8; i++) {
-      const imageUrl = `http://127.0.0.1:8000/media/second_draping/second_${i}.png`;
-      imageUrls.push(imageUrl);
-    }
-
-    // 각 이미지 가져오기
     const getImageData = async () => {
-      for (const url of imageUrls) {
-        try {
-          const response = await fetch(url);
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-
-          const blob = await response.blob();
-          const reader = new FileReader();
-
-          reader.onloadend = () => {
-            // Base64로 변환된 이미지 데이터를 배열에 추가
-            setImageSrcList((prevList) => [...prevList, reader.result]);
-          };
-
-          reader.onerror = (error) => {
-            console.error("이미지를 읽는 중 오류가 발생했습니다:", error);
-          };
-
-          reader.readAsDataURL(blob);
-        } catch (error) {
-          console.error("이미지를 가져오는 중 오류가 발생했습니다:", error);
+      try {
+        const imageUrls = [];
+        for (let i = 1; i <= 8; i++) {
+          const imageUrl = `http://127.0.0.1:8000/media/second_draping/second_${i}.png`;
+          imageUrls.push(imageUrl);
         }
+
+        const responses = await Promise.all(imageUrls.map(url => fetch(url)));
+
+        const imageData = await Promise.all(
+          responses.map(async (response) => {
+            if (!response.ok) {
+              throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            const blob = await response.blob();
+            const reader = new FileReader();
+            return new Promise((resolve, reject) => {
+              reader.onloadend = () => {
+                resolve(reader.result);
+              };
+              reader.onerror = reject;
+              reader.readAsDataURL(blob);
+            });
+          })
+        );
+
+        setImageSrcList(imageData);
+      } catch (error) {
+        console.error("이미지를 가져오는 중 오류가 발생했습니다:", error);
       }
     };
 
     getImageData();
   }, []);
 
-  console.log(imageSrcList);
+  const handleMeasureButtonClick = () => {
+    navigate("/");
+  };
 
   return (
     <div className="outer-container">
       <div className="inner-container">
         <div className="grid-container">
           {imageSrcList.map((src, index) => (
-            <div key={index} className="grid-item-container">  
+            <div key={index} className="grid-item-container">
               <img
                 src={src}
                 alt={`draping ${index + 1}`}
                 className="grid-item"
               />
-            {index === 7 && <p>Second Color</p>}
+              {index === 7 && <p>Second Color</p>}
             </div>
           ))}
         </div>
+        <button id="etc-button" onClick={handleMeasureButtonClick}>처음으로 돌아가기</button>
       </div>
     </div>
   );
 };
 
-export default SecondImageComponent; 
+export default SecondImageComponent;
