6b9550477fbc1186b500033b87d40d59837c4657
yeeunAhn <yeeun780@gmail.com>
2024-05-18 00:23:58 +0900

    result2 css수정

--- src/personal_color/ColorMeasurement.js
+++ src/personal_color/ColorMeasurement.js
@@ -25,11 +25,13 @@ const ColorMeasurement = () => {
     >
       {isClicked ? (
         <div className="measurement-screen">
-          <h2>당신의 퍼스널컬러를 알아보세요</h2>
+          <h2> TZA로 당신의 퍼스널컬러를 알아보세요</h2>
           <p>안경, 모자 등 얼굴을 가리는 것들을 제거해주세요</p>
           <p>얼굴 주변의 머리카락들을 정리해주세요</p>
-          <p1>추후 비식별화된 진단 정보가 연구용으로 사용될 수 있습니다. </p1>
-          <button className="start-button" onClick={handleMeasureButtonClick}>측정하기</button>
+          <p1>*추후 비식별화된 진단 정보가 연구용으로 사용될 수 있습니다. </p1>
+          <button className="start-button" onClick={handleMeasureButtonClick}>
+            측정하기
+          </button>
         </div>
       ) : (
         <p id="click-msg">화면을 클릭하세요</p>
--- src/personal_color/Result2.js
+++ src/personal_color/Result2.js
@@ -42,20 +42,27 @@ const imageMap = {
 };
 
 const colorDescriptions = {
-  "Spring warm light": "밝고 산뜻한 봄의 이미지가 느껴지는 톤입니다.\n 주로 흰색빛의 파스텔 계열이 잘 어울립니다.\n 명도와 채도가 낮은 색은 피해주세요!",
-  "Spring warm bright": "귀엽고 러블리한 이미지가 느껴지는 톤입니다.\n과일처럼 생기 있고 상큼한 이미지에 맞게 고채도의 따뜻한 밝은 색이 잘 어울립니다.\n차갑고 어두운 색은 피해주세요!",
-  "Summer cool light": "이온음료 광고가 떠오르는 여름의 싱그러움이 느껴지는 톤입니다.\n은은하고 부드러운 파스텔 톤이 가장 잘 어울립니다.\n린넨과 쉬폰 소재의 옷을 입으면 이미지를 더 살릴 수 있어요!",
-  "Summer cool mute": "대체로 회색기가 많이 도는 톤다운된 파스텔 계열이 잘 어울립니다.\n깔끔하고 단아한 이미지가 매력적입니다.\n회기가 많이 도는 경우 자칫 칙칙해 보일 수 있어요.",
-  "Autumn warm mute": "부드럽고 차분한 이미지가 떠오르는 톤입니다.\n회기가 돌지만 따뜻한 색이 잘 어울립니다.\n내추럴함이 돋보이는 메이크업과 스타일링이 베스트입니다!",
-  "Autumn warm deep": "고급스럽지만 화려한 느낌이 나는 톤입니다.\n어둡지만 채도가 강한 색들이 잘 어울립니다.\n화려한 주얼리도 잘 어울립니다.",
-  "Winter cool bright": "쿨하지만 화려한 이미지의 톤입니다.\n고채도의 원색에 가까운 쨍한 색이 잘 어울립니다.\n회기 도는 파스텔 계열은 피해주세요.",
-  "Winter cool deep": "시크하고 도도한 이미지가 매력적인 톤입니다.\n진한 레드나 블랙이 누구보다 잘 어울립니다.\n노란 계열과 파스텔 톤은 피해주세요!"
+  "Spring warm light":
+    "밝고 산뜻한 봄의 이미지가 느껴지는 톤입니다.\n 주로 흰색빛의 파스텔 계열이 잘 어울립니다.\n 명도와 채도가 낮은 색은 피해주세요!",
+  "Spring warm bright":
+    "귀엽고 러블리한 이미지가 느껴지는 톤입니다.\n과일처럼 생기 있고 상큼한 이미지에 맞게 고채도의 따뜻한 밝은 색이 잘 어울립니다.\n차갑고 어두운 색은 피해주세요!",
+  "Summer cool light":
+    "이온음료 광고가 떠오르는 여름의 싱그러움이 느껴지는 톤입니다.\n은은하고 부드러운 파스텔 톤이 가장 잘 어울립니다.\n린넨과 쉬폰 소재의 옷을 입으면 이미지를 더 살릴 수 있어요!",
+  "Summer cool mute":
+    "대체로 회색기가 많이 도는 톤다운된 파스텔 계열이 잘 어울립니다.\n깔끔하고 단아한 이미지가 매력적입니다.\n회기가 많이 도는 경우 자칫 칙칙해 보일 수 있어요.",
+  "Autumn warm mute":
+    "부드럽고 차분한 이미지가 떠오르는 톤입니다.\n회기가 돌지만 따뜻한 색이 잘 어울립니다.\n내추럴함이 돋보이는 메이크업과 스타일링이 베스트입니다!",
+  "Autumn warm deep":
+    "고급스럽지만 화려한 느낌이 나는 톤입니다.\n어둡지만 채도가 강한 색들이 잘 어울립니다.\n화려한 주얼리도 잘 어울립니다.",
+  "Winter cool bright":
+    "쿨하지만 화려한 이미지의 톤입니다.\n고채도의 원색에 가까운 쨍한 색이 잘 어울립니다.\n회기 도는 파스텔 계열은 피해주세요.",
+  "Winter cool deep":
+    "시크하고 도도한 이미지가 매력적인 톤입니다.\n진한 레드나 블랙이 누구보다 잘 어울립니다.\n노란 계열과 파스텔 톤은 피해주세요!",
 };
 
 const getImageSrc = (personalColor) => {
-  return `image/${imageMap[personalColor] || 'default_image.png'}`;
+  return `image/${imageMap[personalColor] || "default_image.png"}`;
 };
-
 
 const Result2 = () => {
   const [imageSrc, setImageSrc] = useState("");
@@ -65,7 +72,7 @@ const Result2 = () => {
   });
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
-  
+
   useEffect(() => {
     const imageUrl =
       "http://3.36.217.107/media/cluster_images/total_weighted_mean_color.png";
@@ -109,46 +116,71 @@ const Result2 = () => {
   const handleMeasureButtonClick = () => {
     navigate("/result3");
   };
-  
+
   const goBack = () => {
     navigate(-1);
   };
 
   const displayColor = (color) => {
-    return color.startsWith('N-') ? color.substring(2) : color;
+    return color.startsWith("N-") ? color.substring(2) : color;
   };
 
-  const headerTitle = colorToTitleMap[analysisData.personal_color] || "퍼스널 컬러 진단 결과";
+  const headerTitle =
+    colorToTitleMap[analysisData.personal_color] || "퍼스널 컬러 진단 결과";
 
   return (
     <div className="result2-page">
-      <div className="header-section">
-        <h1>{headerTitle}</h1>
-      </div>
       {loading ? (
         <p>Loading...</p>
       ) : (
         <div className="result2-container">
-          <div className="image-and-qr">
-            <div className="result2-image-section">
-            {analysisData.personal_color.startsWith('N-') ? (
-                <p>당신은 다 잘 어울려요~!<br/> "뉴트럴톤"</p>
-              ) : (
-                <p>당신은<br/>{analysisData.personal_color}</p>
-              )}
-              <img src={imageSrc} alt="Total Weighted Mean Color" className="skin-image" />
+          <div className="result2-first-section">
+            <div className="header-section">
+              <h1>{headerTitle}</h1>
+            </div>
+            <div className="image-and-qr">
+              <div className="result2-image-section">
+                {analysisData.personal_color.startsWith("N-") ? (
+                  <p>
+                    당신은 다 잘 어울려요~!
+                    <br /> "뉴트럴톤"
+                  </p>
+                ) : (
+                  <p>
+                    당신은
+                    <br />
+                    {analysisData.personal_color}
+                  </p>
+                )}
+                <img
+                  src={imageSrc}
+                  alt="Total Weighted Mean Color"
+                  className="skin-image"
+                />
+              </div>
+            </div>
+            <div className="personal-color-graph">
+              <img
+                src={getImageSrc(analysisData.personal_color)}
+                alt={`${analysisData.personal_color}`}
+                style={{ width: "380px", height: "250.5px" }}
+              />
+              <div className="color-descriptions">
+                {analysisData.personal_color.startsWith("N-") ? (
+                  <p>
+                    당신은 웜톤과 쿨톤, 두 가지를 모두 잘 소화할 수 있는
+                    <br /> 특별한 뉴트럴 톤을 가지고 계시네요.
+                    <br /> 더 자유롭게 여러 스타일을 시도할 수 있어요. <br />
+                    뉴트럴 톤이지만, 당신에게 조금 더 조화로운 컬러가 있는데요,
+                    바로 오른쪽에 나와 있는 계절의 톤이에요!
+                  </p>
+                ) : (
+                  <p>{colorDescriptions[analysisData.personal_color]}</p>
+                )}
+              </div>
             </div>
           </div>
-          <div className="personal-color-graph">
-            <img src={getImageSrc(analysisData.personal_color)} alt={`${analysisData.personal_color}`} style={{ width: "380px", height: "250.5px" }} />
-            <div className="color-descriptions">
-            {analysisData.personal_color.startsWith('N-') ? (
-                <p>당신은 웜톤과 쿨톤, 두 가지를 모두 잘 소화할 수 있는<br/> 특별한 뉴트럴 톤을 가지고 계시네요.<br/> 더 자유롭게 여러 스타일을 시도할 수 있어요. <br/>뉴트럴 톤이지만, 당신에게 조금 더 조화로운 컬러가 있는데요, 바로 오른쪽에 나와 있는 계절의 톤이에요!</p>
-              ) : (
-                <p>{colorDescriptions[analysisData.personal_color]}</p>
-              )}
-            </div>
-          </div>
+
           <div className="result2-section">
             <div className="color-info">
               <div className="best-color-section">
@@ -156,14 +188,20 @@ const Result2 = () => {
                   <b>Best : {displayColor(analysisData.personal_color)}</b>
                 </div>
                 <p>가장 잘 어울리는 색상 추천</p>
-                <PersonalColorDiagnosis personalColor={displayColor(analysisData.personal_color)} type="chart" />
+                <PersonalColorDiagnosis
+                  personalColor={displayColor(analysisData.personal_color)}
+                  type="chart"
+                />
               </div>
               <div className="second-color-section">
                 <div className="second-color-title">
-                <b>Second : {displayColor(analysisData.second_color)}</b>
+                  <b>Second : {displayColor(analysisData.second_color)}</b>
                 </div>
                 <p>두번째로 잘 어울리는 색상</p>
-                <PersonalColorDiagnosis personalColor={displayColor(analysisData.second_color)} type="chart" />
+                <PersonalColorDiagnosis
+                  personalColor={displayColor(analysisData.second_color)}
+                  type="chart"
+                />
               </div>
             </div>
           </div>
