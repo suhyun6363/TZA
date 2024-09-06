6180f63440e08463ee5c4eed234a4fd14853100c
gyubb <lgbin910@gmail.com>
2024-09-06 17:46:07 +0900

    h,s,v 최종

--- Web_BE/mycol_app/facemesh.py
+++ Web_BE/mycol_app/facemesh.py
@@ -433,6 +433,7 @@ def process_uploaded_image(uploaded_images):
     L_value = average_lab.lab_l  # L 값
     a_value = average_lab.lab_a  # a 값
     b_value = average_lab.lab_b + 3  # b 값
+    h_value = average_hsv.hsv_h
     s_value = average_hsv.hsv_s * 100 - 20  # s 값
     v_value = average_hsv.hsv_v * 100 + 20  # v 값
 
